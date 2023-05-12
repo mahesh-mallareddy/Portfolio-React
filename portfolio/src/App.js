a4c3ce254702c38c7eb39ffaae86ac645b7f7dca
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-05-12 20:41:41 +0530

    skills ui updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import React from "react";
 import Homeintro from "./components/Home";
+import Skills from "./components/skills";
 
 function App() {
   return (
     <div >
       <Navbar />
       <Homeintro/>
+      <Skills />
     </div>
   );
 }
---
+++ portfolio/src/components/skills.js
@@ -0,0 +1,56 @@
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import java_script from '../assets/javascript.png'
+import react from '../assets/react.png'
+import firebase from '../assets/firebase.png'
+import tailwind from '../assets/tailwind.png'
+import git from '../assets/git.png'
+import mongo from '../assets/mongo.png'
+
+
+
+const Skills = () => {
+
+    const imgList = [
+
+        { name: "html", pic: html, alt: "logo2" },
+
+        { name: "css", pic: css, alt: "logo3" },
+
+        { name: "java script", pic: java_script, alt: "logo4" },
+
+        { name: "react", pic: react, alt: "logo5" },
+
+        { name: "tailwind", pic: tailwind, alt: "logo6" },
+
+        { name: "mongo", pic: mongo, alt: "logo7" },
+
+        { name: "firebase", pic: firebase, alt: "logo8" },
+
+        { name: "git", pic: git, alt: "logo9" }
+
+    ]
+
+    console.log(imgList.map((img) => {
+        return (img.name)
+    }))
+
+    return (
+        <div className='w-full h-[90vh] flex flex-col bg-[#0a192fec]'>
+            <h4 className='text-gray-300 mx-14 '>SKILLS</h4>
+            <p className='text-gray-400 mx-16 my-1' >Technologies I use in my development</p>
+            <div className='w-fit mx-auto'>
+                <div className='grid grid-cols-2 md:grid-cols-3 my-3'>
+                    {imgList.map((img) => {
+                        return (<div className='mx-5 my-1 sm:my-5 sm:mx-10 shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                            < img src={img.pic} className='w-12 md:w-16 mx-8 my-3' />
+                            <p className='w-fit mx-auto my-1 text-gray-400'>{img.name}</p>
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Skills;
