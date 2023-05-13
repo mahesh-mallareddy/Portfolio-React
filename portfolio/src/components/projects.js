6fd6bbe38b93f2dc0ce192fafa9b0941eefa5a1e
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-05-13 13:31:55 +0530

    modified ui styles

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -30,26 +30,25 @@ const Project = () => {
                     {
                         Projectdata.map((data) => {
                             return (
-                                <div className='group w-fit md:mx-10'>
-                                    <div className=' my-3  shadow-md shadow-[#040c16] hover:scale-110 duration-300  group-hover:shadow-none'>
-
-                                        <div className='project_bg  mx-5 my-2  flex justify-center items-center rounded-lg ' style={{ backgroundImage: `url(${data.img})` }}>
-                                            <div className='opacity-0 group-hover:opacity-100 duration-300'>
+                                <div className='group w-fit md:mx-10 my-2 shadow-md shadow-[#040c16] hover:shadow-none  duration-700'>
+                                        <div className='project_bg  mx-5 my-2  flex justify-center items-center rounded-lg  hover:scale-110 duration-500 ' style={{ backgroundImage: `url(${data.img})` }}>
+                                            <div className='opacity-0 group-hover:opacity-100 duration-300 my-5'>
                                                 <a>
                                                     <button className='rounded-md mx-8 px-4 py-1 bg-teal-400
-                                                    '>Demo</button>
+                                                    duration-500'>Demo</button>
                                                 </a>
                                                 <a>
-                                                    <button className='mx-8 px-4 py-1 bg-teal-400 rounded-md '>code</button>
+                                                    <button className='mx-8 px-4 py-1 bg-teal-400 rounded-md duration-500'>code</button>
                                                 </a>
                                             </div>
                                         </div>
-                                        <div className='w-fit mx-auto text-center my-4  opacity-100 group-hover:opacity-0'>
+                                        <div className='w-fit mx-auto text-center my-2  opacity-100 group-hover:opacity-0 duration-300'>
                                             <h4 className='text-gray-300 '>{data.name}</h4>
                                             <p className='text-gray-300 text-sm '>using {data.desc}</p>
                                         </div>
                                     </div>
-                                </div>)
+                               
+                                )
                         })
                     }
                 </div>
